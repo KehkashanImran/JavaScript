030330eaf0146c46d88a805a667ce60a719d917f
kehkashan <kehkashanimran110@gmail.com>
2023-04-06 18:25:11 +0500

    code upload

---
+++ scriptFile/jsFile.js
@@ -0,0 +1,5 @@
+var zakatPercentage=0.025;
+var userInput= prompt("Enter your Amount for calculating zakat");
+userInput=Number(userInput);
+var result=zakatPercentage*userInput;
+alert("Your Zakat Amount is " + result);
